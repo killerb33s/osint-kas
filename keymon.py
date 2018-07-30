8b826996eb2afaf1d1407fe9b5eb7fa7d3137380
killerb33s <41686273+killerb33s@users.noreply.github.com>
2018-07-30 12:42:36 -0400

    Update keymon.py

--- keymon.py
+++ keymon.py
@@ -46,7 +46,7 @@ def createDir(name):
     cwd = os.getcwd()
     directory = cwd + "/%s" % (name)
     if not os.path.exists(directory):
-        os.makedirs(directory,0644)
+        os.makedirs(directory,0755)
         print "[*] Directory For %s Not Found" % (name)
         print "[!] Directory For %s Created..." % (name)
     else:
